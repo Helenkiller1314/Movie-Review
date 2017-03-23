307ffd88b1e84e9c203a549354cfb08080fafc1e
Helen <helenmayanna@gmail.com>
2017-03-23 21:20:41 +0800

    implement movie favorite

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,7 @@ class Movie < ApplicationRecord
   validates :title, presence: true
   belongs_to :user
   has_many :reviews
+
+  has_many :movie_favorites
+  has_many :members, through: :movie_favorites, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :movies
   has_many :reviews
+  has_many :movie_favorites
+  has_many :participated_movies, :through => :movie_favorites, :source => :movie
 end
