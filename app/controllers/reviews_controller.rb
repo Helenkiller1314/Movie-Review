7304bfee12eb693f93e57a23f5543fa31fdb4ade
Helen <helenmayanna@gmail.com>
2017-03-24 21:10:54 +0800

    make edit and delete for my reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,9 +6,30 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
+  def edit
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to account_reviews_path, notice: "影评更新成功！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.destroy(params[:id])
+    @review.destroy
+    redirect_to account_reviews_path, alert:"影评被删除！"
+
+  end
+
   def create
-
-
     @review = Review.new(review_params)
     @review.movie = @movie
     @review.user = current_user
